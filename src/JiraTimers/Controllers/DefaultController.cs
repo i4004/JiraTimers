b017d84fafb83d82ec03ecf0de45bceebf286a3a
Alexanderius <alexander.krylkov@gmail.com>
2019-09-30 22:13:27 +0600

    Initial Simplify.Web project

---
+++ src/JiraTimers/Controllers/DefaultController.cs
@@ -0,0 +1,15 @@
+﻿using Simplify.Web;
+using Simplify.Web.Attributes;
+using Simplify.Web.Responses;
+
+namespace JiraTimers.Controllers
+{
+	[Get("/")]
+	public class DefaultController : Controller
+	{
+		public override ControllerResponse Invoke()
+		{
+			return new Ajax("Hello world");
+		}
+	}
+}
---
+++ src/JiraTimers/Startup.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Simplify.Web.Owin;
+
+namespace JiraTimers
+{
+	public class Startup
+	{
+		public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+		{
+			if (env.IsDevelopment())
+				app.UseDeveloperExceptionPage();
+
+			app.UseSimplifyWeb();
+		}
+	}
+}
