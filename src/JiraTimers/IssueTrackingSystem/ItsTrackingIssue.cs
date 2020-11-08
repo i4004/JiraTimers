cf3d7e32df72489187664381ec9e85f12955fae2
Alexanderius <alexander.krylkov@gmail.com>
2020-11-08 19:56:31 +0600

    Notify fix

--- src/JiraTimers/IssueTrackingSystem/ItsTrackingIssue.cs
+++ src/JiraTimers/IssueTrackingSystem/ItsTrackingIssue.cs
@@ -1,7 +1,10 @@
-﻿namespace JiraTimers.IssueTrackingSystem
+﻿using Qml.Net;
+
+namespace JiraTimers.IssueTrackingSystem
 {
 	public class ItsTrackingIssue : IItsTrackingIssue
 	{
+		[NotifySignal]
 		public string Name { get; set; }
 	}
 }
