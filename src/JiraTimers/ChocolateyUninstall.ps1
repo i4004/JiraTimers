cee5755c0548fb96909a4314c52faefe0fa7c03c
Alexanderius <alexander.krylkov@gmail.com>
2020-11-28 00:06:51 +0600

    Choco uninstall script

---
+++ src/JiraTimers/ChocolateyUninstall.ps1
@@ -0,0 +1,8 @@
+﻿$linkName = "JiraTimers.lnk"
+$programsPath = [environment]::GetFolderPath([environment+specialfolder]::Programs)
+$shortcutFilePath = Join-Path $programsPath $linkName
+
+if (Test-Path $shortcutFilePath)
+{
+	Remove-Item $shortcutFilePath
+}
