be41b70f20e012617a0387a4e5eb45d8e4f1f55d
Alexanderius <alexander.krylkov@gmail.com>
2020-11-28 00:42:26 +0600

    Version retrieve fix

--- Publish.ps1
+++ Publish.ps1
@@ -1,23 +1,15 @@
 # Paths
 
 $solutionFilePath = "src/JiraTimers.sln"
-$projectPath = ".\src\JiraTimers"
-$nuspecFilePath = Join-Path $projectPath "jiratimers.nuspec"
+$localProjectPath = ".\src\JiraTimers"
+$projectPath = "\src\JiraTimers"
+$nuspecFilePath = Join-Path $localProjectPath "jiratimers.nuspec"
 $winReleasePath = Join-Path $projectPath "publish-win"
 $workingDirectory = Get-Location
 $fullReleasePath = Join-Path $workingDirectory $winReleasePath
 $dllPath = Join-Path $fullReleasePath "JiraTimers.dll"
 $publishProfileName = "Release-Win"
-
-function GetVersion
-{
-	$assembly = [Reflection.Assembly]::Loadfile($dllPath)
-
-	$assemblyName = $assembly.GetName()
-	return $assemblyName.version
-}
-
-$version = GetVersion
+$version = [Reflection.AssemblyName]::GetAssemblyName($dllPath).Version.ToString()
 
 # Publish
 dotnet publish $solutionFilePath -p:PublishProfile=$publishProfileName
