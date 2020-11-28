7741ee56f319b1b0e646f5ef5fecc393cda5a057
Alexanderius <alexander.krylkov@gmail.com>
2020-11-28 17:25:26 +0600

    Build messages added

--- Publish.ps1
+++ Publish.ps1
@@ -18,19 +18,19 @@ $dllPath = Join-Path $fullReleasePath "JiraTimers.dll"
 $publishProfileName = "Release-Win"
 $setupProjectPath = "src/JiraTimers.Setup/JiraTimers.Setup.vdproj"
 
-# Publish
+"1/4: Build windows release..."
 dotnet publish $solutionFilePath -p:PublishProfile=$publishProfileName
 
 # Retrieve app version
 $version = [Reflection.AssemblyName]::GetAssemblyName($dllPath).Version.ToString()
 
-# Patch MSI project version
+"2/4: Patch MSI project version..."
 ((Get-Content -Path $setupProjectPath -Raw) -replace '{version}', $version) | Set-Content -Path $setupProjectPath
 
-# Build MSI version
+"3/4: Build MSI version..."
 & "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\IDE\devenv.com" $setupProjectPath /build Release /projectconfig Release
 
-# Pack Choco package
+"3/4: Pack Choco package..."
 choco pack $nuspecFilePath --version $version
 
 exit $LastExitCode
